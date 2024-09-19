f56fea4a5176de0ca1aaa48ff19448e25f476684
StruzhakS <StruzhakS@gmail.com>
2024-09-19 16:15:36 +0300

    fixed code for best indexation by google

--- src/layout/mainHead/MainHead.js
+++ src/layout/mainHead/MainHead.js
@@ -5,14 +5,18 @@ const MainHead = props => {
   const { title, noindex, description, canonical = false } = props;
 
   const { t } = useTranslation();
+  // console.log(props);
 
-  const host = "https://test.com";
+  const host = "https://remtent.com";
   return (
     <Head>
       <title>{t(title)}</title>
       <meta name="description" content={t(description)} />
+      <meta name="google-site-verification" content="ET72KFLMagR2NE3O_8BsZSS2dgq8kqb08THhiyC_GgI" />
+
       {noindex && <meta name="robots" content="noindex, nofollow" />}
-      <link rel="shortcut icon" href="/public/favicon/favicon.ico" type="image/x-icon" />
+      {/* <link rel="icon" href="/SS_icon.svg" /> */}
+      {/* <link rel="icon" href="/favicon.ico" type="image/x-icon" /> */}
       {canonical && <link rel="canonical" href={`${host}${canonical}`}></link>}
     </Head>
   );
